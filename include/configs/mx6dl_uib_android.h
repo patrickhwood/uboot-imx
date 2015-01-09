8563ec186a58f6bd1addf64398f7c304446e5822
Pat Wood <Pat.Wood@efi.com>
2015-01-08 19:59:30 -0500

    Set up kernel boot parameters for LVDS0.

--- include/configs/mx6dl_uib_android.h
+++ include/configs/mx6dl_uib_android.h
@@ -121,7 +121,8 @@
 		"kernel=uImage\0"													\
 		"nfsroot=/opt/eldk/arm\0"											\
 		"bootargs_base=setenv bootargs console=ttymxc0,115200 init=/init "	\
-		"video=mxcfb0:dev=ldb,bpp=32 video=mxcfb1:off video=mxcfb2:off fbmem=10M " \
+		"video=mxcfb0:dev=ldb,LDB-WSVGA,bpp=32 ldb=dul0 "							\
+		"video=mxcfb1:off video=mxcfb2:off fbmem=10M "						\
 		"fb0base=0x27b00000 vmalloc=400M androidboot.console=ttymxc0 "		\
 		"androidboot.hardware=freescale androidboot.serialno=0a01234567890abc\0" \
 		"bootargs_nfs=setenv bootargs ${bootargs} root=/dev/nfs "			\
